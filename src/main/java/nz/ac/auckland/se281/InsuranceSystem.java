e6d74ffb0791857ed88c461a0bcae2b8de386b1d
Talha Ashraf <tash192@aucklanduni.ac.nz>
2023-03-17 09:30:12 +1300

    added Person file and completed some logic for printdb

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,18 +1,40 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
+  ArrayList<Person> peopleList = new ArrayList();
+  
+
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
   }
 
   public void printDatabase() {
+    int numberOfProfiles = peopleList.size();
+    if(numberOfProfiles == 0){
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0","s",".");
+
+    }
+    else if(numberOfProfiles == 1){
+
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1",".");
+    }
+    else{
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage((Integer.toString(numberOfProfiles)), "s", ":" );
+
+    }
+
     // TODO: Complete this method.
   }
 
   public void createNewProfile(String userName, String age) {
     // TODO: Complete this method.
+    
+    Person newPerson = new Person(userName, age);
+    peopleList.add(newPerson);
   }
 
   public void loadProfile(String userName) {
---
+++ src/main/java/nz/ac/auckland/se281/Person.java
@@ -0,0 +1,11 @@
+package nz.ac.auckland.se281;
+
+public class Person {
+    String userName;
+    String age;
+
+    public Person(String userName, String age){
+        this.userName = userName;
+        this.age = age;
+    }
+}
